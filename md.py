eabc0590149a567a4737be990cf9a9c957fd0a84
longcy1111 <2504413116@qq.com>
2024-06-25 17:41:04 +0800

    Update md.py

--- md.py
+++ md.py
@@ -21,6 +21,10 @@ else:
 path = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(path,"face_shapes_model.pkl")
 
+
+from fastprogress import fastprogress 
+# 禁用fastai的进度条 
+fastprogress.fastprogress.NO_BAR = True
 # 加载模型
 learn_inf = load_learner(model_path)
 
